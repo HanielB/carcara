71b19e6378d1c67734950a24f7981c01bfce0885
Haniel Barbosa <hanielbbarbosa@gmail.com>
2025-09-22 15:07:52 -0700

    support pow2

--- carcara/src/parser/mod.rs
+++ carcara/src/parser/mod.rs
@@ -503,6 +503,10 @@ impl<'a, R: BufRead> Parser<'a, R> {
                 }
                 SortError::assert_all_eq(&sorts)?;
             }
+            Operator::Pow2 => {
+                assert_num_args(&args, 1)?;
+                SortError::assert_eq(&Sort::Int, sorts[0])?;
+            }
             Operator::RareList => SortError::assert_all_eq(&sorts)?,
         }
         Ok(self.pool.add(Term::Op(op, args)))
