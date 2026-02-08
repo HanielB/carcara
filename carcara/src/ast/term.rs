98eccde8ecaba0cc51bdb5dbc64ad0da7e15cfbc
Haniel Barbosa <hanielbbarbosa@gmail.com>
2026-02-08 19:26:17 -0300

    missing bvxor in aci_simp

--- carcara/src/ast/term.rs
+++ carcara/src/ast/term.rs
@@ -1056,6 +1056,7 @@ impl Operator {
             | Operator::BvOr
             | Operator::BvMul
             | Operator::BvAnd
+            | Operator::BvXor
             | Operator::BvConcat => true,
             _ => false,
         }
