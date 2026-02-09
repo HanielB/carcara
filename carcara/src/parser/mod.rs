7ccd9187a8979f00b039b548723c18ab9fee659c
Haniel Barbosa <hanielbbarbosa@gmail.com>
2026-02-09 15:45:37 -0300

    fix for rare_list parsing

--- carcara/src/parser/mod.rs
+++ carcara/src/parser/mod.rs
@@ -523,7 +523,10 @@ impl<'a, R: BufRead> Parser<'a, R> {
                 assert_num_args(&args, 1)?;
                 SortError::assert_eq(&Sort::Int, sorts[0])?;
             }
-            Operator::RareList => SortError::assert_all_eq(&sorts)?,
+            // Note that we cannot enforce that the sorts of the
+            // arguments are the same because for example for BV rules
+            // the types may differ
+            Operator::RareList => {},
         }
         Ok(self.pool.add(Term::Op(op, args)))
     }
